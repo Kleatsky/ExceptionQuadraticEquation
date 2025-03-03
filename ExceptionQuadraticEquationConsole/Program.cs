ba2777a0144e07de41b7761fcabe6bc2d2ba83f5
salaga999999999 <salaga999999999@gmail.com>
2025-03-03 14:28:48 +0700

    Create Solution

---
+++ ExceptionQuadraticEquationConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionQuadraticEquationConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
